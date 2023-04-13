f5ff848dffc5792fcfe1dfee560020d7963f75d6
Anton Volvenko <volvenko.anthony@gmail.com>
2023-04-13 11:06:46 +0300

    added some comments to JS

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,13 @@
+//* Обираємо список Catogories з li-elements всередині
+
 const categoryList = document.querySelector('#categories');
 const categoryItems = document.querySelectorAll('li.item');
 
+//* Виводомо в консоль кількість категорій
+
 console.log(`Number of categories: ${categoryItems.length}`);
+
+//! Перебираємо для кожного елементу списку
 
 categoryItems.forEach((item) => {
   const categoryTitle = item.querySelector('h2').textContent;
--- js/task-02.js
+++ js/task-02.js
@@ -8,6 +8,8 @@ const ingredients = [
 ];
 const ingredientsList = document.querySelector('#ingredients');
 
+//* Перебираємо старий та створюємо новий масив і виводимо значення в список
+
 const ingredientsItems = ingredients.map((ingredient) => {
   const listOfItems = document.createElement('li');
   listOfItems.textContent = ingredient;
